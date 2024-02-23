5df42a73b633d47dc93d96516115df2b2040f351
FlorianPERI <florian.peri@oclock.school>
2024-02-23 17:23:57 +0100

    feat: home

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 import 'dotenv/config';
 import Debug from 'debug';
 import router from './src/app.router.js';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 import express from 'express';
 
@@ -35,6 +39,10 @@ app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(openapiSpecification));
 
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    const htmlFilePath = join(__dirname, './src/views/index.html');
+    res.sendFile(htmlFilePath);
+});
 app.use(router);
 
 const PORT = process.env.PORT ?? 3000;
